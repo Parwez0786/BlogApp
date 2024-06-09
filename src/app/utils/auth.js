e953e589baca362dfb2373df0821e5b49f825a58
91810 <parwezansari1024@gmail.com>
2024-06-09 15:32:43 +0530

    Initial commit1

--- src/app/utils/auth.js
+++ src/app/utils/auth.js
@@ -8,13 +8,7 @@ import { getServerSession } from "next-auth";
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GitHub({
-
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     Google({
-      
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
